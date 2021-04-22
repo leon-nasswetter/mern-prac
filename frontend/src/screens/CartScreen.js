6c8786fc3e4ace2fa8e872877dcb1d64935a2661
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-04-21 19:12:24 -0700

    wip

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,11 @@
+import "./CartScreen.css"
+
+const CartScreen = () => {
+    return (
+        <div className="cartscreen">
+            Cartscreen
+        </div>
+    )
+}
+
+export default CartScreen
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import "./ProductScreen.css"
+
+const ProductScreen = () => {
+    return (
+        <div className="productscreen">
+            Productscreen
+        </div>
+    )
+}
+
+export default ProductScreen
